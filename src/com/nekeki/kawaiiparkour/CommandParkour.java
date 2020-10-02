5893de3da6049575e3198ebd22d500b9c0e466ef
Kevin1722000 <kevin1722000@gmail.com>
2020-10-03 00:30:45 +0200

    refactored commands -> simplified function depth

--- src/com/nekeki/kawaiiparkour/CommandParkour.java
+++ src/com/nekeki/kawaiiparkour/CommandParkour.java
@@ -5,6 +5,8 @@ import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 public class CommandParkour implements CommandExecutor {
+    private final String NO_PLAYER_MESSAGE = "You need to be a player to use this command, silly!";
+    private final String NO_PERMISSION_MESSAGE = ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.";
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -87,7 +89,7 @@ public class CommandParkour implements CommandExecutor {
             TimeManager.playerSetup((Player) sender);
             TimeManager.commandReset((Player) sender);
         } else {
-            sender.sendMessage("You need to be a player to use this command, silly!");
+            sender.sendMessage(this.NO_PLAYER_MESSAGE);
         }
     }
 
@@ -97,7 +99,7 @@ public class CommandParkour implements CommandExecutor {
             TimeManager.playerSetup((Player) sender);
             TimeManager.commandCheckpoint((Player) sender);
         } else {
-            sender.sendMessage("You need to be a player to use this command, silly!");
+            sender.sendMessage(this.NO_PLAYER_MESSAGE);
         }
     }
 
@@ -115,31 +117,32 @@ public class CommandParkour implements CommandExecutor {
         if(sender.hasPermission("KawaiiParkour.list") || sender.hasPermission("KawaiiParkour.admin")) {
             TimeManager.sendParkourList(sender);
         } else {
-            sender.sendMessage(ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.");
+            sender.sendMessage(this.NO_PERMISSION_MESSAGE);
         }
     }
 
     private void createAction(CommandSender sender, String[] args) {
         //Creates a parkour course
-        if(sender.hasPermission("KawaiiParkour.create") || sender.hasPermission("KawaiiParkour.admin")) {
-            if(sender instanceof Player) {
-                if(args.length > 1) {
-                    if(args[1].equals("null")) {
-                        sender.sendMessage(ChatColor.RED + "You can't name your course that, sorry!");
-                    } else {
-                        sender.sendMessage(ChatColor.DARK_PURPLE + "Started parkour creation for " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + ". Please run through your course. Iron Plate = Start, Stone Plate = Checkpoint, Gold Plate = Finish");
-                        TimeManager.playerSetup((Player) sender);
-                        TimeManager.parkourSetup((Player) sender, args[1]);
-                    }
-                } else {
-                    sender.sendMessage(ChatColor.RED + "Please include a name for the parkour!");
-                }
-            } else {
-                sender.sendMessage("You need to be a player to use this command, silly!");
-            }
-        } else {
-            sender.sendMessage(ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.");
+        if(!sender.hasPermission("KawaiiParkour.create") && !sender.hasPermission("KawaiiParkour.admin")) {
+            sender.sendMessage(this.NO_PERMISSION_MESSAGE);
+            return;
         }
+        if(!(sender instanceof Player)) {
+            sender.sendMessage(this.NO_PLAYER_MESSAGE);
+            return;
+        }
+        if(args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Please include a name for the parkour!");
+            return;
+        }
+        if(args[1].equals("null")) {
+            sender.sendMessage(ChatColor.RED + "You can't name your course that, sorry!");
+            return;
+        }
+
+        sender.sendMessage(ChatColor.DARK_PURPLE + "Started parkour creation for " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + ". Please run through your course. Iron Plate = Start, Stone Plate = Checkpoint, Gold Plate = Finish");
+        TimeManager.playerSetup((Player) sender);
+        TimeManager.parkourSetup((Player) sender, args[1]);
     }
 
     private void cancelAction(CommandSender sender) {
@@ -148,91 +151,96 @@ public class CommandParkour implements CommandExecutor {
             TimeManager.playerSetup((Player) sender);
             TimeManager.cancelSetup((Player) sender);
         } else {
-            sender.sendMessage("You need to be a player to use this command, silly!");
+            sender.sendMessage(this.NO_PLAYER_MESSAGE);
         }
     }
 
     private void deleteAction(CommandSender sender, String[] args) {
         //Deletes parkour course
-        if(sender.hasPermission("KawaiiParkour.delete") || sender.hasPermission("KawaiiParkour.admin")) {
-            if(args.length > 1) {
-                if(sender instanceof Player) {
-                    TimeManager.playerSetup((Player) sender);
-                    if(TimeManager.preDeleteParkour(args[1], (Player) sender)) {
-                        sender.sendMessage(ChatColor.DARK_PURPLE + "Are you sure you want to delete " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + "? This will delete all times. Use " + ChatColor.LIGHT_PURPLE + "/parkour confirm " + ChatColor.DARK_PURPLE + "to confirm.");
-                    } else {
-                        sender.sendMessage(ChatColor.RED + "That is not a parkour! This is case sensitive.");
-                    }
-                } else {
-                    TimeManager.deleteParkour(args[1], true);
-                }
+        if(!sender.hasPermission("KawaiiParkour.delete") && !sender.hasPermission("KawaiiParkour.admin")) {
+            sender.sendMessage(this.NO_PERMISSION_MESSAGE);
+            return;
+        }
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "You need to include a course name to delete!");
+            return;
+        }
+
+        if(sender instanceof Player) {
+            TimeManager.playerSetup((Player) sender);
+            if(TimeManager.preDeleteParkour(args[1], (Player) sender)) {
+                sender.sendMessage(ChatColor.DARK_PURPLE + "Are you sure you want to delete " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + "? This will delete all times. Use " + ChatColor.LIGHT_PURPLE + "/parkour confirm " + ChatColor.DARK_PURPLE + "to confirm.");
             } else {
-                sender.sendMessage(ChatColor.RED + "You need to include a course name to delete!");
+                sender.sendMessage(ChatColor.RED + "That is not a parkour! This is case sensitive.");
             }
         } else {
-            sender.sendMessage(ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.");
+            TimeManager.deleteParkour(args[1], true);
         }
     }
 
     private void confirmAction(CommandSender sender) {
         //Confirms parkour deletion
-        if(sender.hasPermission("KawaiiParkour.delete") || sender.hasPermission("KawaiiParkour.admin")) {
-            if(sender instanceof Player) {
-                TimeManager.playerSetup((Player) sender);
-                if(TimeManager.getToBeDeleted((Player) sender).equals("null")) {
-                    sender.sendMessage(ChatColor.RED + "You are not in the deletion process.");
-                } else {
-                    TimeManager.deleteParkour(TimeManager.getToBeDeleted((Player) sender), false);
-                    sender.sendMessage(ChatColor.DARK_PURPLE + "Deleted parkour " + ChatColor.LIGHT_PURPLE + TimeManager.getToBeDeleted((Player) sender) + ChatColor.DARK_PURPLE + ".");
-                    TimeManager.resetToBeDeleted((Player) sender);
-                }
-            } else {
-                sender.sendMessage("You need to be a player to use this command, silly!");
-            }
+        if(!sender.hasPermission("KawaiiParkour.delete") && !sender.hasPermission("KawaiiParkour.admin")) {
+            sender.sendMessage(this.NO_PERMISSION_MESSAGE);
+            return;
+        }
+        if(!(sender instanceof Player)) {
+            sender.sendMessage(this.NO_PLAYER_MESSAGE);
+            return;
+        }
+
+        TimeManager.playerSetup((Player) sender);
+        if(TimeManager.getToBeDeleted((Player) sender).equals("null")) {
+            sender.sendMessage(ChatColor.RED + "You are not in the deletion process.");
         } else {
-            sender.sendMessage(ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.");
+            TimeManager.deleteParkour(TimeManager.getToBeDeleted((Player) sender), false);
+            sender.sendMessage(ChatColor.DARK_PURPLE + "Deleted parkour " + ChatColor.LIGHT_PURPLE + TimeManager.getToBeDeleted((Player) sender) + ChatColor.DARK_PURPLE + ".");
+            TimeManager.resetToBeDeleted((Player) sender);
         }
     }
 
     private void clearTimesAction(CommandSender sender, String[] args) {
         //Clears all times from course
-        if(sender.hasPermission("KawaiiParkour.cleartimes") || sender.hasPermission("KawaiiParkour.admin")) {
-            if(args.length > 1) {
-                TimeManager.deleteAllTimes(args[1]);
-                sender.sendMessage(ChatColor.DARK_PURPLE + "Deleted all personal bests and record time for " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + ".");
-            } else {
-                sender.sendMessage(ChatColor.RED + "Please enter a course name to clear all times from!");
-            }
-        } else {
-            sender.sendMessage(ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.");
+        if(!sender.hasPermission("KawaiiParkour.cleartimes") && !sender.hasPermission("KawaiiParkour.admin")) {
+            sender.sendMessage(this.NO_PERMISSION_MESSAGE);
+            return;
         }
+        if(args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Please enter a course name to clear all times from!");
+            return;
+        }
+
+        TimeManager.deleteAllTimes(args[1]);
+        sender.sendMessage(ChatColor.DARK_PURPLE + "Deleted all personal bests and record time for " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + ".");
     }
 
     private void clearTopTimeAction(CommandSender sender, String[] args) {
         //Clears record time from course
-        if(sender.hasPermission("KawaiiParkour.cleartimes") || sender.hasPermission("KawaiiParkour.admin")) {
-            if(args.length > 1) {
-                TimeManager.deleteRecordTime(args[1]);
-                sender.sendMessage(ChatColor.DARK_PURPLE + "Deleted record time for " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + ".");
-            } else {
-                sender.sendMessage(ChatColor.RED + "Please enter a course name to clear top time from!");
-            }
-        } else {
-            sender.sendMessage(ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.");
+        if(!sender.hasPermission("KawaiiParkour.cleartimes") && !sender.hasPermission("KawaiiParkour.admin")) {
+            sender.sendMessage(this.NO_PERMISSION_MESSAGE);
+            return;
         }
+        if(args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Please enter a course name to clear top time from!");
+            return;
+        }
+
+        TimeManager.deleteRecordTime(args[1]);
+        sender.sendMessage(ChatColor.DARK_PURPLE + "Deleted record time for " + ChatColor.LIGHT_PURPLE + args[1] + ChatColor.DARK_PURPLE + ".");
     }
 
     private void optionAction(CommandSender sender, String[] args) {
         //Changes plugin options
-        if(sender.hasPermission("KawaiiParkour.option") || sender.hasPermission("KawaiiParkour.admin")) {
-            if(args.length > 1) {
-                ConfigurablesExecutor configurablesExecutor = new ConfigurablesExecutor();
-                configurablesExecutor.setOption(sender, args[1]);
-            } else {
-                sender.sendMessage(ChatColor.RED + "Please include an option! Current options: firework, sound");
-            }
-        } else {
-            sender.sendMessage(ChatColor.RED + "Heck! I'm sorry. You don't have permission to run this command.");
+        if(!sender.hasPermission("KawaiiParkour.option") && !sender.hasPermission("KawaiiParkour.admin")) {
+            sender.sendMessage(this.NO_PERMISSION_MESSAGE);
+            return;
         }
+        if(args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Please include an option! Current options: firework, sound");
+            return;
+        }
+
+        ConfigurablesExecutor configurablesExecutor = new ConfigurablesExecutor();
+        configurablesExecutor.setOption(sender, args[1]);
     }
 }
